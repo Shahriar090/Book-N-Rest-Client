7855e07462140251749e4055fb96622de22ccd50
Shahriar <shahriarh655@gmail.com>
2024-08-31 18:52:06 +0600

    Replaced all color values with CSS variables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,14 @@ const Header = () => {
       <nav className="w-full h-full max-w-screen-xl mx-auto flex items-center justify-between px-2">
         <div className="logo">
           <Link to="/">
-            <span className="text-white font-medium text-2xl">BookNRest</span>
+            <span className="text-primary-text font-medium text-2xl">
+              BookNRest
+            </span>
           </Link>
         </div>
         <div className="btn">
           <Link to="/sign-in">
-            <button className="px-6 py-3 text-blue-600 font-medium bg-white rounded-md hover:bg-gray-100">
+            <button className="px-6 py-3 text-blue-600 font-medium bg-primary-text rounded-md hover:bg-secondary-text">
               SignIn
             </button>
           </Link>
--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -11,10 +11,10 @@ const HeroBanner = () => {
 
         <div className="absolute top-[40%] sm:left-[20%] sm:right-[20%] transform translate(-50%, -50%)">
           <div className="flex flex-col gap-3">
-            <h1 className="text-white text-3xl sm:text-5xl md:text-6xl text-center font-semibold">
+            <h1 className="text-primary-text text-3xl sm:text-5xl md:text-6xl text-center font-semibold">
               The Best Luxury Hotels
             </h1>
-            <p className="text-gray-100 font-medium text-sm text-center">
+            <p className="text-secondary-text font-medium text-sm text-center">
               Discover Pure Luxury In Every Corner Of Our Hotel.Experience
               unparalleled elegance and comfort in our meticulously designed
               rooms, each offering a serene escape from the everyday. Indulge in
@@ -23,7 +23,7 @@ const HeroBanner = () => {
               to your every need.
             </p>
             <div className="flex justify-center">
-              <button className="px-6 py-3 text-blue-600 font-medium bg-white rounded-md hover:bg-gray-100">
+              <button className="px-6 py-3 text-primary-color font-medium bg-primary-text rounded-md hover:bg-secondary-text">
                 Book Now
               </button>
             </div>

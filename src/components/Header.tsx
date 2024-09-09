4ea540c57f3c73f7faae4c082d44d16ff522bdba
Shahriar <shahriarh655@gmail.com>
2024-09-09 13:12:04 +0600

    Made the navigation bar responsive for both mobile and large devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { MdMenu, MdClose } from "react-icons/md";
 const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+
+  // nav options
   const navItems = [
     { label: "Home", link: "/" },
     { label: "Our Rooms", link: "/our-rooms" },
@@ -13,14 +21,18 @@ const Header = () => {
         {/* logo */}
         <div className="logo">
           <Link to="/">
-            <span className="text-primary-text font-semibold text-3xl">
+            <span className="text-primary-text font-semibold text-2xl md:text-3xl">
               BookNRest
             </span>
           </Link>
         </div>
         {/* nav options */}
-        <div className="">
-          <ul className="flex gap-6 text-primary-text text-lg font-semibold">
+        <div>
+          <ul
+            className={`flex flex-col md:flex-row fixed left-0 text-center md:static z-[-1] md:z-auto w-full h-auto transition-all duration-500 ease-in-out gap-6 text-primary-text text-lg font-semibold bg-primary-color py-8 md:py-0 ${
+              isMobileMenuOpen ? "top-20" : "-top-[500px]"
+            }`}
+          >
             {navItems.map((item, index) => (
               <li
                 className="hover:text-secondary-text transition-all duration-300"
@@ -29,14 +41,40 @@ const Header = () => {
                 <Link to={item.link}>{item.label}</Link>
               </li>
             ))}
+
+            <div className="md:hidden">
+              <Link to="/sign-in">
+                <button className="px-6 py-3 text-primary-color font-semibold bg-primary-text rounded-md hover:bg-secondary-text">
+                  SignIn
+                </button>
+              </Link>
+            </div>
           </ul>
         </div>
-        <div className="btn">
+        <div className="btn hidden md:block">
           <Link to="/sign-in">
             <button className="px-6 py-3 text-primary-color font-semibold bg-primary-text rounded-md hover:bg-secondary-text">
               SignIn
             </button>
           </Link>
+        </div>
+
+        {/* for small device */}
+        <div className="md:hidden">
+          <button
+            className="text-2xl text-primary-text"
+            onClick={handleDrawerToggle}
+          >
+            {isMobileMenuOpen ? (
+              <span>
+                <MdClose />
+              </span>
+            ) : (
+              <span>
+                <MdMenu />
+              </span>
+            )}
+          </button>
         </div>
       </nav>
     </header>

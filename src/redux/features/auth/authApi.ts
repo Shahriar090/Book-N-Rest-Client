3bd8d27491e0fe25e1ea5784f7a1ad786f88e0fb
Shahriar <shahriarh655@gmail.com>
2024-09-15 16:41:06 +0600

    Implemented seperate api with inject end points to maintain clean base api

--- src/redux/features/auth/authApi.ts
+++ src/redux/features/auth/authApi.ts
@@ -1,1 +1,15 @@
- 
+import baseApi from "@/redux/api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (userInfo) => ({
+        url: "/user/register-user",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation } = authApi;

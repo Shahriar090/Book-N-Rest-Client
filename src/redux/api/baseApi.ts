f9ba0a03a1c873dd53de52606a7eafcb110dba45
Shahriar <shahriarh655@gmail.com>
2024-09-30 00:28:12 +0600

    Modified the Redux base API and implemented a custom API request to refresh the access token. If the refresh token is expired, the user must log in again to continue browsing

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { userLogin, userLogout } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:7000/api/v1",
@@ -13,9 +14,33 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+// custom base query for refresh token
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status === 401) {
+    const res = await fetch(
+      "http://localhost:7000/api/v1/user/refresh-access-token",
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(userLogin({ user, accessToken: data?.data?.accessToken }));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(userLogout());
+    }
+  }
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
 
